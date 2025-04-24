ab2610379534b9719df21ae83d6162aaf55f382a
Jorge Toloza <jorgetloza@gmail.com>
2025-04-24 12:00:34 -0500

    :tada: Init

---
+++ src/js/pages/index.js
@@ -0,0 +1,23 @@
+import '../../styles/index.scss';
+import '../../styles/pages/index.scss';
+
+export default class Index {
+  constructor() {
+    window.addEventListener('resize', this.resize.bind(this));
+    this.resize();
+    this.initGrid();
+  }
+  initGrid() {
+    document.addEventListener('keydown', (e) => {
+      if(e.shiftKey && e.key === 'G') {
+        document.getElementById('grid').classList.toggle('show')
+      }
+    })
+  }
+  resize() {
+    document.documentElement.style.setProperty('--rvw', `${document.documentElement.clientWidth / 100}px`);
+  }
+}
+window.addEventListener('load', () => {
+  new Index();
+});
